53f5f1a4d0a6240e82e08c84c31f4ea0c33a1111
Valadmin <lengothanhthinh@gmail.com>
2023-02-26 21:29:48 +0700

    Handle to user login.

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -5,22 +5,20 @@ import { Request, Response } from 'express';
 import { Repository } from 'typeorm';
 
 export class AuthController {
-  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
-    return bcrypt.compareSync(unencryptedPassword, this.password);
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string, password: string): boolean {
+    return bcrypt.compareSync(unencryptedPassword, password);
   }
+
   async login(req: Request, res: Response) {
     const repository = AppDataSource.getRepository(User);
     const { email, password } = req.body;
-    const hashPassword;
-    const user = await repository
-      .createQueryBuilder('u')
-      .where('u.email = :email', { email })
-      // .andWhere('u.password = :password', { password })
-      .getOne();
+    const user = await repository.createQueryBuilder('u').where('u.email = :email', { email }).getOne();
     if (!user) {
       return res.status(401).json({ message: 'Login Fail' });
-    } else {
-      return res.status(200).json({ message: 'Login Success' });
     }
+    if (!this.checkIfUnencryptedPasswordIsValid(password, user.password)) {
+      return res.status(401).json({ message: 'Login Fail' });
+    }
+    return res.status(200).json({ message: 'Login Success' });
   }
 }
